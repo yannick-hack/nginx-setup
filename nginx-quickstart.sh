35b6d0a369e46a1c62b3f8535080a28c1b91c69d
yannick-hack <60113494+yannick-hack@users.noreply.github.com>
2020-01-20 21:30:33 +0100

    Update nginx-quickstart.sh

--- nginx-quickstart.sh
+++ nginx-quickstart.sh
@@ -2,3 +2,4 @@ apt -y update
 apt -y install nginx
 openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 ln -s /etc/nginx/sites-available/nginx_keycloak.conf /etc/nginx/sites-enabled/
+systemctl restart nginx
